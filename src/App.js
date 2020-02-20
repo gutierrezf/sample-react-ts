e81c783058aa648d58daa20fddd5ed45efef8f90
Francis Gutierrez <francis_gutierrez@condenast.com>
2020-02-20 15:16:29 -0400

    sample react project js

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import "./App.css";
+import TodoList from "./components/TodoList";
+import Counter from "./components/Counter";
+
+class App extends Component {
+  state = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Counter count={this.state.count} increment={increment} decrement={decrement}/>
+        <TodoList />
+      </div>
+    );
+  }
+}
+
+export default App;
